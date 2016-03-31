49a564ba2119196fdbee3b3adfc9785721bb0228
Nakwon Lee <skrdnjs0904@naver.com>
2016-04-01 02:17:27 +0900

    during CNF

--- cnf.py
+++ cnf.py
@@ -194,23 +194,31 @@ def CNF(root):
 	key = root.getSym()
 
 	if key is 2: # root is "or"
-		leftkey = root.left.getSym()
-		rightkey = root.right.getSym()
-
-		if leftkey is 1: # left child is "and"
-			f1 = root.left.left
-			f2 = root.left.right
-			f3 = root.right
-
-			root.left.right = f3
-			root.right = BinaryNode(2)
-
-			root.right.left = f2
-			root.right.right = CopyTree(f3)
-
-		elif (leftkey is not 1) and rightkey is  
+		  
 
 	return root
+
+def DISTR(root)
+	leftkey = root.left.getSym()
+	rightkey = root.right.getSym()
+
+	if leftkey is 1: # left child is "and"
+		f1 = root.left.left
+		f2 = root.left.right
+		f3 = root.right
+
+		root.left.right = f3
+		root.right = BinaryNode(1)
+
+		root.right.left = f2
+		root.right.right = CopyTree(f3)
+
+		root.setSym(1)
+		root.left.setSym(2)
+
+	elif (leftkey is not 1) and rightkey is
+
+	return root 
 
 if __name__ == '__main__':
 
